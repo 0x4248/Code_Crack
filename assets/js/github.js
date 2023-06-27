360ed6834e6db6cd930f4f63b6727780750b1947
lewisevans2007 <awesomelewis2007@users.noreply.github.com>
2023-06-27 08:41:06 +0100

    Username update
    Signed-off-by: lewisevans2007 <awesomelewis2007@users.noreply.github.com>

--- assets/js/github.js
+++ assets/js/github.js
@@ -2,8 +2,8 @@ async function githubshow(){
     document.getElementById("footer").innerHTML = "Click me to go to my GitHub page"
 }
 async function githubhide(){
-    document.getElementById("footer").innerHTML = "Made by awesomelewis2007"
+    document.getElementById("footer").innerHTML = "Made by lewisevans2007"
 }
 async function gotogithub(){
-    window.location.href = "https://www.github.com/awesomelewis2007"
+    window.location.href = "https://www.github.com/lewisevans2007"
 }
