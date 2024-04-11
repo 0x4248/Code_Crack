ba572833f5ff2d74e20f7cc24baa14be88df2a96
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-04-11 11:55:46 +0100

    Username update
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

--- assets/js/github.js
+++ assets/js/github.js
@@ -2,8 +2,8 @@ async function githubshow(){
     document.getElementById("footer").innerHTML = "Click me to go to my GitHub page"
 }
 async function githubhide(){
-    document.getElementById("footer").innerHTML = "Made by lewisevans2007"
+    document.getElementById("footer").innerHTML = "Made by 0x4248"
 }
 async function gotogithub(){
-    window.location.href = "https://www.github.com/lewisevans2007"
+    window.location.href = "https://www.github.com/0x4248"
 }
